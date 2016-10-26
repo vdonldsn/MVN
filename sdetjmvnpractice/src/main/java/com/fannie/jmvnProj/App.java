18773a1978ea7e165da6801c66f4d02a04c6ffd0
Virgil Donaldson <vdonldsn@gmail.com>
2016-10-26 17:10:16 -0400

    initial commit for jenkins maven job

---
+++ sdetjmvnpractice/src/main/java/com/fannie/jmvnProj/App.java
@@ -0,0 +1,13 @@
+package com.fannie.jmvnProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage( "Hello World!" );
+    }
+}
---
+++ sdetjmvnpractice/src/main/java/com/fannie/jmvnProj/Util.java
@@ -0,0 +1,7 @@
+package com.fannie.jmvnProj;
+
+public class Util{
+	public static void printMessage(String message){
+		System.out.println(message);
+	}
+}
